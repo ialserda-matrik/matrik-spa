90f0f8a4b3236f5c280171e43056f37b7c6b00d7
Ivo Alserda <info@matrik.nl>
2016-02-08 18:01:04 +0100

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name matrikSpaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the matrikSpaApp
+ */
+angular.module('matrikSpaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
